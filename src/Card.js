9a9a8d5cb998df706accdd3cfc0627202cdc28b9
anishlp7 <anishlp7@gmail.com>
2018-06-02 12:18:59 +0530

    Adding Components CardList & loop

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
     return(
         <div className="tc bg-light-green dib br3 pa3 bw2 grow ma2 shadow-5">
-            <img alt="Photo" src={`https://robohash.org/${props.id}?200x200`} />
+            <img alt="Robots" src={`https://robohash.org/${props.id}?200x200`} />
         <div>
             <h2>{props.name}</h2>
             <p>{props.email}</p>
